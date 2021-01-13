ffda0afabb607696a725951e7f105759f4e9ae8b
Srul1k <srul1k.post@ya.ru>
2021-01-13 11:45:07 +0300

    Add Invoker

---
+++ ppc/ppc/Invoker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using ppc.Commands;
+
+namespace ppc
+{
+    class Invoker
+    {
+        private Stack<ICommand> _history;
+        private ICommand _command;
+
+        public Invoker()
+        {
+            _history = new Stack<ICommand>();
+        }
+
+        public void SetCommand(ICommand command)
+        {
+            _command = command;
+        }
+
+        public void Run()
+        {
+            _command.Execute();
+            if (_command is ICancelable)
+            {
+                _history.Push(_command);
+            }
+        }
+
+        public void Undo()
+        {
+            _command = _history.Pop();
+            var command = _command as ICancelable;
+            command.Undo();
+        }
+    }
+}
