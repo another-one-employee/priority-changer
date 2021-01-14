2cea44b66527fd84a631756247e715fbb681f872
Srul1k <srul1k.post@ya.ru>
2021-01-14 20:57:25 +0300

    Update Program.cs
    Main logic implemented

--- ppc/ppc/Program.cs
+++ ppc/ppc/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ppc.Commands;
+using System;
+using System.Linq;
 
 namespace ppc
 {
@@ -6,7 +8,74 @@ namespace ppc
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                var help = new HelpCommand();
+                help.Execute();
+                return;
+            }
 
+            Invoker invoker = new Invoker();
+            int level;
+            string key;
+
+            switch (args.First())
+            {
+                case "c":
+                case "create":
+                    level = CpuLevelStringToInt(args.Last());
+                    key = ExtractingKeyFromArguments(args, 1, 1);
+                    invoker.SetCommand(new CreateCommand(key, level));
+                    break;
+
+                case "r":
+                case "read":
+                    invoker.SetCommand(new ReadAllCommand());
+                    break;
+
+                case "u":
+                case "update":
+                    level = CpuLevelStringToInt(args.Last());
+                    key = ExtractingKeyFromArguments(args, 1, 1);
+                    invoker.SetCommand(new UpdateCommand(key, level));
+                    break;
+
+                case "d":
+                case "delete":
+                    key = ExtractingKeyFromArguments(args, 1, 0);
+                    invoker.SetCommand(new DeleteCommand(key));
+                    break;
+
+                case "undo":
+                    invoker.Undo();
+                    return;
+
+                default:
+                    invoker.SetCommand(new HelpCommand());
+                    break;
+            }
+
+            invoker.Run();
+        }
+
+        private static int CpuLevelStringToInt(string value)
+        {
+            int level;
+            if (!Int32.TryParse(value, out level))
+            {
+                level = (int)Enum.Parse(typeof(CpuPriorityLevel), value);
+            }
+
+            return level;
+        }
+
+        private static string ExtractingKeyFromArguments(string[] args, int countOfCommandWords, int countOfOtherArgs)
+        {
+            var temp = args
+                .TakeWhile((x, i) => i != args.Length - countOfOtherArgs)
+                .Skip(countOfCommandWords);
+
+            return String.Join(" ", temp);
         }
     }
 }
