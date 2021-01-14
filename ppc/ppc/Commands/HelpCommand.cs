fea74ef94de862a2bdee8a596b3edff2f10e8da0
Srul1k <srul1k.post@ya.ru>
2021-01-14 20:56:49 +0300

    Add HelpCommand

---
+++ ppc/ppc/Commands/HelpCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ppc.Commands
+{
+    class HelpCommand : ICommand
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Some information"); //add description
+        }
+    }
+}
